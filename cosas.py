b1a74fa3a50bc191536bbd716582d23f3d4d6671
AriGochi <125834764+AriGochi@users.noreply.github.com>
2023-06-15 14:12:35 -0600

    Add files via upload

--- cosas.py
+++ cosas.py
@@ -1,97 +1,100 @@
-class Alumno:
- facultad = "FES Aragón"
-
-def __init__(self, nom, ed, carr):
-    self.__nombre = nom
-    self.__edad = ed
-    self.__carrera = carr
-
-def set_nombre(self, nom):
-    self.__nombre = nom
-
-def get_nombre(self):
-    return self.__nombre
-
-def set_edad(self, ed):
-    self.__edad = ed
-
-def get_edad(self):
-    return self.__edad
-
-def set_carrera(self, car):
-    self.__carrera = car
-
-def get_carrera(self):
-    return self.__carrera
-
-def __str__(self):
-    cadena = "___Nombre: " +self.__nombre
-    cadena = cadena + "Edad: " +str(self.__edad)
-    cadena = cadena + "Carrera" + str(self.__carrera)
-    cadena = cadena + "-------------"
-    return  cadena
-
-def estudiar (self,  horas = 1):
-    print( f"El alumno {self.__nombre} esta estudiando {horas}")
-
-class Perro:
-    reino = "Canino"
-
-    def __int__(self, raza, edad, estatura):
-        self.__raza =raza
-        self.__edad = edad
-        self.__estatura = estatura
-
-    #metodo de acceso get
+class Autor:
+    def __init__(self, nom, pseudo):
+        self.__nombre = nom
+        self.__pseudonimo = pseudo
 
     @property
-    def raza (self)
-        return  self.__raza
+    def nombre(self):
+        return self.__nombre
 
-    @raza.setter
-    def raza (self, la_raza):
-        self.__raza = la_raza
-
-    @property
-    def edad(self):
-        return self.__edad
-
-    @edad.setter
-    def edad(self, la_edad):
-        if la_edad>0 and la_edad<20:
-            self.__edad =la_edad
-
-        else:
-            print("Esa no es una edad vañlida")
-            self.__edad = 0
+    @nombre.setter
+    def nombre(self, nom):
+        self.__nombre = nom
 
     @property
-    def estatura(self):
-        return  self.__estatura
+    def pseudonimo(self):
+        return self.__pseudonimo
 
-    @estatura.setter
-    def estatura(self, la_estatura):
-        if la_estatura > 0.1 and la_estatura < 1.1:
-            self.__estatura = la_estatura
-
-        else:
-            print("No way")
-            self.__estatura = 0.1
+    @pseudonimo.setter
+    def pseudonimo(self,pseudo):
+        self.__pseudonimo = pseudo
 
     def __str__(self):
-        return f""
+        return f"Nombre {self.__nombre} pseudonimo: {self.__pseudonimo}"
 
-    @staticmethod
-    def es_cachorro(edad):
-        return edad <3
+    def escribir(self):
+        print(f"{self.__pseudonimo} esta escribiendo su siguiente libro")
 
-    @staticmethod
-    def dormir(veces = 5):
-        for n in range(veces):
-            print(f"Dando vuelta{ n +1}")
-            print("zzzz")
+class Libro:
+    def __init__(self, tit, aut, an, ed):
+        self.__titulo = tit
+        self.__autor = aut
+        self.__año = an
+        self.__editorial = ed
 
-    @staticmethod
-    def perro_grande(cls, est):
-        if est >0.79:
-            return cls("",0,est)
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self,tit):
+        self.__titulo = tit
+
+    @property
+    def autor(self):
+        return self.__autor
+
+    @autor.setter
+    def autor(self,aut):
+        self.__autor = aut
+
+    @property
+    def año(self):
+        return self.__año
+
+    @año.setter
+    def año(self, an):
+        self.__año = an
+
+    @property
+    def editorial(self):
+        return self.__editorial
+
+    @editorial.setter
+    def editorial(self, ed):
+        self.__editorial = ed
+
+    def __str__(self):
+        return f"""""
+            Titulo = {self.__titulo}
+            Autor = {self.__autor}
+            Año ={self.__año}
+        Editorial ={self.__editorial}
+        """
+
+    @classmethod
+    def libro_planeta(cls, titulo, autor, año):
+        return cls(titulo, autor, año, "Planeta")
+
+    def leer(self, minutos):
+        print(f"Leyendo por {minutos} minutos")
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.__nombre = nombre
+        self.__edad = edad
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, nom):
+        self.__nombre = nom
+
+class Alumno(Persona):
+    def __init__(self, nombre, edad, numer_cuenta, carrera, promedio):
+        super().__init__(nombre, edad)
+        self.__numero_cuenta = numer_cuenta
+        self.__carrera = carrera
+        self.__promedio = promedio
--- main.py
+++ main.py
@@ -1,32 +1,13 @@
-from cosas import Alumno
-
+from cosas import *
 
 def main():
-    al1 = Alumno("Jose",19,"ICO")
-    print(vars(al1))
-    al1.__nombre = "Diego"
-    print(vars(al1))
-    al1.set_nombre("Ari")
-    print(vars(al1))
-    print("-------To String----")
-    print(al1)
-    al1.set_edad(999)
-    print(al1)
-    al1.estudiar(4)
-    print("_____Perro---")
-    perro1 = Perro("Podle", 2, 0.35)
-    print(vars(perro1))
-    perro1.raza = "De la calle"
-    print(vars(perro1))
-    perro1.__raza = "otra"
-    perro1.edad = 12
-    perro1.estatura = 0.43
-    print(perro1)
-    cachorro = Perro.es_cachorro(perro1.edad)
-    print(cachorro)
-    Perro.dormir()
-    danes = Perro.perro_grande(0.8)
-    print(danes)
-
-
+    l1 = Libro.libro_planeta("EL PERFUME", Autor("Patrik", "PS"), 1980)
+    print(l1)
+    l1.autor.pseudonimo = l1.autor.pseudonimo.lower()
+    print(l1)
+    print("------Herencia-----")
+    al2 = Alumno("JOse", 19,"324443", "iCO", 9)
+    al2.nombre = "Pepe"
+    print(vars(al2))
+    
 main()
